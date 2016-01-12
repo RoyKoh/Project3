3166237b5130ceb9a2d0af6b1c7cfc8635ff5d70
RoyKoh <i168168@yahoo.com.sg>
2016-01-12 12:26:22 +0800

    Modi app.js package.json

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express')
 var app = express()
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/dist'))
 var port = process.env.PORT || 5000
 app.listen(port)
